36932a413060a521a987de187e726d178ae74df6
NKShukla <naval19065@iiitd.ac.com>
2021-10-16 23:58:26 +0530

    Added Date - Seller Page

--- django-react-ecommerce-master/src/pages/sellerPages/components/Seller_Deposits.js
+++ django-react-ecommerce-master/src/pages/sellerPages/components/Seller_Deposits.js
@@ -1,38 +1,39 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Grid } from '@material-ui/core';
 
 function Title(props) {
-    return (
-      <Typography component="h2" variant="h6" color="primary" gutterBottom style={{marginTop:"10px"}}>
-        {props.children}
-      </Typography>
-    );
-  }
+  return (
+    <Typography component="h2" variant="h6" color="primary" gutterBottom style={{ marginTop: "10px" }}>
+      {props.children}
+    </Typography>
+  );
+}
 
-const getCurrentDate=()=>{
 
+export default function Seller_Deposits() {
+  const [currDate, setCurrDate] = useState("- - -");
+
+  useEffect(() => {
     var date = new Date().getDate();
     var month = new Date().getMonth() + 1;
     var year = new Date().getFullYear();
 
-    var ans= (date + '-' + month + '-' + year);
-    console.log(ans);
-    return ans.toString;
-}
+    setCurrDate(date + '-' + month + '-' + year);
+  }, [setCurrDate]);
 
-export default function Seller_Deposits() {
-    
   return (
-    <Grid style={{margin:"30px"}}>
+    <Grid style={{ margin: "30px" }}>
       <Title>Recent Deposits</Title>
       <Typography component="p" variant="h6">
         $3,024.00
       </Typography>
       <Typography color="text.secondary" sx={{ flex: 1 }}>
-        {getCurrentDate}
+        {currDate}
       </Typography>
-      
+
     </Grid>
   );
 }
