1fdffd42dc56cb6202965b13418645296ecdbf25
Shanu <63155782+shanu19104@users.noreply.github.com>
2021-10-18 01:46:34 +0530

    Deleted Folder

--- django-react-ecommerce-master/myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ashita/Downloads/django-react-ecommerce-master/myenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
