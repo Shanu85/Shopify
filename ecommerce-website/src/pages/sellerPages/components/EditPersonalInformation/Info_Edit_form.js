d45ebc624713549c7f8235406d3860d135a81354
Shanu <63155782+shanu19104@users.noreply.github.com>
2021-10-19 00:56:03 +0530

    Added new form

--- ecommerce-website/src/pages/sellerPages/components/EditPersonalInformation/Info_Edit_form.js
+++ ecommerce-website/src/pages/sellerPages/components/EditPersonalInformation/Info_Edit_form.js
@@ -1,10 +1,10 @@
 import React from 'react'
-<<<<<<< HEAD
 import { Grid,Paper,Button} from '@material-ui/core'
 import { useState } from 'react';
 import {email_reg} from '../../../authPages/regexes';
 import { useDispatch } from "react-redux";
 import {updateSellerInfo} from '../../../../redux/actions/authActions'
+import useReactRouter from "use-react-router";
 
 function Info_Edit_form({First_Name,Last_Name,Phone_No,National_Code,email}) {
     const paperStyle = { padding: '0 15px 40px 15px', width: 450, }
@@ -17,6 +17,7 @@ function Info_Edit_form({First_Name,Last_Name,Phone_No,National_Code,email}) {
     const [NationalCode,setNationalCode]=useState(National_Code);
     const [Email,setEmail]=useState(email);
     
+    const { history } = useReactRouter();
 
     function isNumber(n) { return !isNaN(n) }
 
@@ -53,46 +54,14 @@ function Info_Edit_form({First_Name,Last_Name,Phone_No,National_Code,email}) {
         }
         const user={FirstName,LastName,PhoneNo,NationalCode,Email};
 
-        // dispatch(updateSellerInfo(user,history));
+        dispatch(updateSellerInfo(user,history));
         
         alert(`${FirstName} ${LastName} ${PhoneNo} ${NationalCode} ${Email}`)
-=======
-import { Formik, Form, Field, ErrorMessage } from 'formik'
-import * as Yup from 'yup'
-import { TextField, Grid, Paper, Button } from '@material-ui/core'
-import { phone_number_reg, national_code_reg } from "../../../authPages/regexes";
-
-function Info_Edit_form({ First_Name, Last_Name, Phone_No, National_Code, Email }) {
-    const paperStyle = { padding: '0 15px 40px 15px', width: 450, }
-    const btnStyle = { marginTop: 20 }
-    const initialValues = {
-        first_name: First_Name,
-        last_name: Last_Name,
-        phone: Phone_No,
-        code: National_Code,
-        email: Email
-    }
-
-    //yup.string().test('len', 'Must be exactly 5 characters', val => val.length === 5)
-    const validationSchema = Yup.object().shape({
-        first_name: Yup.string().test('len', 'Please enter First Name', val => val.length >= 1),
-        last_name: Yup.string().test('len', 'Please enter Last Name', val => val.length >= 1),
-        phone: Yup.string().matches(phone_number_reg, "Invalid phone number").required("Required field"),
-        email: Yup.string().email().required("Required field"),
-        code: Yup.string().matches(national_code_reg, "Invalid National Code"),
-    })
-
-    const onSubmit = ({ first_name, last_name, phone, code, email }, { setErrors, resetForm }) => {
-        alert(first_name);
-        console.log(first_name);
-        resetForm()
->>>>>>> dfff0854055c8759ac3b07a4d0e622ccb6aa3666
     }
 
     return (
         <Grid>
             <Paper elevation={0} style={paperStyle}>
-<<<<<<< HEAD
             <form onSubmit={handleSubmit}>
                 <Grid container spacing={2}>
                     
@@ -142,32 +111,6 @@ function Info_Edit_form({ First_Name, Last_Name, Phone_No, National_Code, Email 
                         setEmail(event.target.value)
                     }} required/>
                     </Grid>
-=======
-
-                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-                    {props => (
-                        <Form onSubmit={onSubmit}>
-
-                            <Field as={TextField} name='first_name' label="First Name" placeholder={initialValues.first_name} fullWidth
-                                error={props.errors.first_name && props.touched.first_name}
-                                helperText={<ErrorMessage name='first_name' />} required style={{ marginTop: "10px" }} />
-
-                            <Field as={TextField} name='last_name' label="Last Name" placeholder={initialValues.last_name} fullWidth
-                                error={props.errors.last_name && props.touched.last_name}
-                                helperText={<ErrorMessage name='last_name' />} required style={{ marginTop: "10px" }} />
-
-                            <Field as={TextField} name='phone' label="Phone Number" placeholder={initialValues.phone} fullWidth
-                                error={Boolean(props.errors.phone) || Boolean(props.errors.non_field_errors)}
-                                helperText={<ErrorMessage name='phone' />} required style={{ marginTop: "10px" }} />
-
-                            <Field as={TextField} name='code' label="National Code" placeholder={initialValues.code} fullWidth
-                                error={Boolean(props.errors.phone) || Boolean(props.errors.non_field_errors)}
-                                helperText={<ErrorMessage name='code' />} required style={{ marginTop: "10px" }} />
-
-                            <Field as={TextField} name='email' label="Email" placeholder={initialValues.email} fullWidth
-                                error={props.errors.email && props.touched.email}
-                                helperText={<ErrorMessage name='email' />} required style={{ marginTop: "10px" }} />
->>>>>>> dfff0854055c8759ac3b07a4d0e622ccb6aa3666
 
                     
                     <Grid item xs={6}></Grid>
@@ -182,9 +125,5 @@ function Info_Edit_form({ First_Name, Last_Name, Phone_No, National_Code, Email 
     )
 }
 
-<<<<<<< HEAD
 export default Info_Edit_form
 
-=======
-export default Info_Edit_form
->>>>>>> dfff0854055c8759ac3b07a4d0e622ccb6aa3666
