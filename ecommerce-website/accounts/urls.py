6d9caac9688fc1565049e223e7e827614328283b
Jasdeep Singh <81826357+Jassi-71@users.noreply.github.com>
2021-10-19 15:47:38 +0530

    changes in model

--- ecommerce-website/accounts/urls.py
+++ ecommerce-website/accounts/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('change-password/', views.ChangePasswordView.as_view(),
-         name='change-password'),
+    path('change-password/', views.ChangePasswordView.as_view(), name='change-password'),
 ]
